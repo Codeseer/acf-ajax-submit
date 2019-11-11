e23bb9800c82221e286b6b7e37fa867a495ab160
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2019-11-11 11:21:49 +0600

    Spinner Hide/Show
    The spinner was not hiding after ajax success and not showing when ajax submit

--- acfAjax.js
+++ acfAjax.js
@@ -1,19 +1,18 @@
 //listens to all acf_forms on page.
 jQuery(function($) {
   acf.add_filter('validation_complete', function( json, form ) {
-  	if( !json.errors ) {
+    if( !json.errors ) {
       //if no errors stop form from being submitted.
       form.submit(function(event) { event.preventDefault(); submitACF_AJAX(this); return false;});
-  	}
+    }
     return json;
   });
 
   //sends the request using FormData object will work with file uploads as well.
   function submitACF_AJAX(form) {
-  	var data = new FormData(form);
+    var data = new FormData(form);
     var $spinner = $("#acf-form").find('.spinner, .acf-spinner');
-
-  	$.ajax({
+    $.ajax({
       type: 'POST',
       url: window.location.href,
       data: data,
@@ -22,12 +21,12 @@ jQuery(function($) {
       beforeSend: function(data){
         acf.showSpinner( $spinner );
       }
-  	})
-  	.done(function(data) {
-  		$(form).trigger('acf_submit_complete', data);
+    })
+    .done(function(data) {
+      $(form).trigger('acf_submit_complete', data);
       acf.hideSpinner( $spinner );
     })
-  	.fail(function(error) {
+    .fail(function(error) {
       $(form).trigger('acf_submit_fail', error);
     });
   }
