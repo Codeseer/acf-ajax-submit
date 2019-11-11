bd615784dfef72ac578faf72046517e3897b0f55
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2019-11-11 11:18:59 +0600

    fixed: Show hide spinner

--- acfAjax.js
+++ acfAjax.js
@@ -11,15 +11,21 @@ jQuery(function($) {
   //sends the request using FormData object will work with file uploads as well.
   function submitACF_AJAX(form) {
   	var data = new FormData(form);
+    var $spinner = $("#acf-form").find('.spinner, .acf-spinner');
+
   	$.ajax({
       type: 'POST',
       url: window.location.href,
       data: data,
       processData: false,
-      contentType: false
+      contentType: false,
+      beforeSend: function(data){
+        acf.showSpinner( $spinner );
+      }
   	})
   	.done(function(data) {
   		$(form).trigger('acf_submit_complete', data);
+      acf.hideSpinner( $spinner );
     })
   	.fail(function(error) {
       $(form).trigger('acf_submit_fail', error);
